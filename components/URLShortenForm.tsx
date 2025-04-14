70278d42c0f5211484b172d53b954bdf124758ff
Steven Phung <sphung01@bu.edu>
2025-04-14 12:18:53 -0400

    Database is working so far

--- components/URLShortenForm.tsx
+++ components/URLShortenForm.tsx
@@ -1,28 +1,43 @@
 "use client"
 import { Button } from "@mui/material";
+import { useState } from "react";
+import createShortURL from "@/lib/createShortURL";
 
 export default function URLShortenForm() {
+    const [url, setUrl] = useState("");
+
+    const handleUrl = (event: React.FormEvent) => {
+        event.preventDefault();
+        console.log(url);
+        createShortURL(url);
+    }
+
     return(
-        <form>
-            <div className="flex flex-col items-center justify-center bg-green-400 w-100 h-75 rounded-md">
-                <input
-                    className="text-black text-center text-lg rounded-md 
-                    border-round border-groove border-4
-                    bg-white w-75 h-10"
-                    placeholder="Enter your URL here"
-                    required
-                />
-                <Button 
-                    type="submit"
-                    sx={{
-                        bgcolor: "black",
-                        color: "white",
-                        width: "150px",
-                        margin: "4px"
-                        }}>
-                    Shorten URL!
-                </Button>
-            </div>
+        <form 
+            className="flex flex-col items-center justify-center 
+                bg-green-400 w-100 h-75 rounded-md"
+            onSubmit={handleUrl}
+            >
+            <input
+                className="text-black text-center text-lg rounded-md 
+                border-round border-groove border-4
+                bg-white w-75 h-10"
+                value={url}
+                type="url"
+                placeholder="Enter your URL here"
+                onChange={(e) => setUrl(e.target.value)}
+                required
+            />
+            <Button 
+                type="submit"
+                sx={{
+                    bgcolor: "black",
+                    color: "white",
+                    width: "150px",
+                    margin: "4px"
+                    }}>
+                Shorten URL!
+            </Button>
         </form>
     );
 }
