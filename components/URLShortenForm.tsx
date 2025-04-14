be7b9341383c8395d9690109aa1fb1c356ef5049
Steven Phung <sphung01@bu.edu>
2025-04-14 11:51:53 -0400

    Deleted some files

--- components/URLShortenForm.tsx
+++ components/URLShortenForm.tsx
@@ -1,13 +1,27 @@
 "use client"
-import SubmitButton from "./SubmitButton";
-import InputBox from "./InputBox";
+import { Button } from "@mui/material";
 
 export default function URLShortenForm() {
     return(
         <form>
             <div className="flex flex-col items-center justify-center bg-green-400 w-100 h-75 rounded-md">
-                <InputBox />
-                <SubmitButton />
+                <input
+                    className="text-black text-center text-lg rounded-md 
+                    border-round border-groove border-4
+                    bg-white w-75 h-10"
+                    placeholder="Enter your URL here"
+                    required
+                />
+                <Button 
+                    type="submit"
+                    sx={{
+                        bgcolor: "black",
+                        color: "white",
+                        width: "150px",
+                        margin: "4px"
+                        }}>
+                    Shorten URL!
+                </Button>
             </div>
         </form>
     );
