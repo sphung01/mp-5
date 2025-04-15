fb3a6ec7ee8fe0cfe097bd1966bf9d8c1d4fb76a
Steven Phung <sphung01@bu.edu>
2025-04-15 10:28:01 -0400

    Removed one line from header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,4 @@
 export default function Header() {
-  const linkStyling = "p-1 m-2 text-xl hover:underline";
   return (
     <header className="flex justify-between items-center h-20">
       <h2 className="text-4xl font-semibold p-4">URL Shortener Website</h2>
